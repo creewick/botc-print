20d83da568e96780dd4beb165a9151d726a2a06e
creewick <ilia_Y@mail.ru>
2026-03-23 11:28:21 +0500

    recommended travellers

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
+const firstPageTypes = {
+    "2bfc6e58-485f-8006-9d07-fe360e067c6b": "Горожане",
+    "2bfc6e58-485f-804b-a4f2-fe76108367bc": "Изгои",
+    "2bfc6e58-485f-80b4-b28c-d3f87189b821": "Приспешники",
+    "2bfc6e58-485f-8059-b37f-d6cffbccbf6c": "Демоны",
+}
+const secondPageTypes = {
+    "2bfc6e58-485f-8048-884c-fa29f3dbe5d6": "Летописцы",
+    "2bfc6e58-485f-8072-9a69-e77527250fcf": "Сказочники",
+    "2bfc6e58-485f-80d8-87ae-eb92d2bb40b3": "Странники",
+}
+const recommendedTravellers = ['thief', 'harlot', 'judge', 'beggar', 'scapegoat']
+
 async function onInit() {
     const { ids, title } = parseQuery()
     const roles = await getJson('./roles.json')
-    const types = await getJson('./types.json')
+    const travellers = filterRoles(roles, recommendedTravellers)
     const filteredRoles = filterRoles(roles, ids)
     const systemRoles = filterRolesByType(roles, undefined)
 
     renderTitle(title)
-    renderRoles(filteredRoles, types)
-    renderNightOrder([...filteredRoles, ...systemRoles])
+    getRoleSections(filteredRoles, firstPageTypes).forEach(section => 
+        document.getElementsByClassName('page')[0].appendChild(section))
+    getRoleSections([...filteredRoles, ...travellers], secondPageTypes).forEach(section => 
+        document.getElementsByClassName('page')[1].insertBefore(section, document.getElementById('players-count')))
+    // renderNightOrder([...filteredRoles, ...systemRoles])
 }
 
 function parseQuery() {
@@ -44,11 +60,11 @@ function renderTitle(title) {
     document.getElementById("script-title").innerText = title ?? 'Сценарий'
 }
 
-function renderRoles(roles, types) {
-    Object.entries(types).forEach(([id, name]) => {
+function getRoleSections(roles, types) {
+    return Object.entries(types).map(([id, name]) => {
         const rolesByType = filterRolesByType(roles, id)
-        renderRoleType(rolesByType, name, id)
-    })
+        return renderRoleType(rolesByType, name, id)
+    }).filter(x => x)
 }
 
 function renderRoleType(roles, name, id) {
@@ -65,8 +81,8 @@ function renderRoleType(roles, name, id) {
   
     section.appendChild(renderTypeTitle(name))
     section.appendChild(renderRoleList(roles))
-    
-    document.getElementsByClassName('page')[0].appendChild(section)
+
+    return section
 }
 
 function renderTypeTitle(name) {
@@ -112,6 +128,19 @@ function getRoleOrder(index, total) {
     }
 
     return (index - half) * 2 + 1
+}
+
+function renderTravellers(roles) {
+    if (roles.length === 0) return
+
+    const section = document.createElement('section')
+    section.classList.add('roleType', 'traveller')
+    section.style.flexGrow = roles.length;
+  
+    section.appendChild(renderTypeTitle(secondPageTypes['2bfc6e58-485f-80d8-87ae-eb92d2bb40b3']))
+    section.appendChild(renderRoleList(roles))
+    
+    document.getElementsByClassName('page')[1].insertBefore(section, document.getElementById('players-count'))
 }
 
 function renderNightOrder(roles) {
